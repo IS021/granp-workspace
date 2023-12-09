55152bd4b877f577367e276280acab4c31a778d2
MariarosaMarchese <mariarosamarchese2@gmail.com>
2023-12-09 18:35:37 +0100

    Fixed bugs

--- projects/granp-lib/src/models/Profession.ts
+++ projects/granp-lib/src/models/Profession.ts
@@ -1,16 +1,7 @@
 export enum Profession {
-<<<<<<< HEAD
     Doctor,
     Nurse,
     Physiotherapist,
     Caregiver,
     Other
     }
-=======
-    Doctor = "Medico",
-    Nurse = "Infermiere",
-    Physiotherapist = "Fisioterapista",
-    Caregiver = "Badante",
-    Other = "Altro"
-}
->>>>>>> 3f54ecec093e0c75d30a14b20ba80f5ed203e8a0
