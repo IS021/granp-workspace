45bc452e5b1fb4ecda9399bb58e0da2f10bfb248
Antonio Pio Maggio <a.maggio4@studenti.poliba.it>
2023-12-23 12:10:25 +0100

    Fixed chat bug

--- projects/granp-lib/src/lib/chat-page/chat.page.ts
+++ projects/granp-lib/src/lib/chat-page/chat.page.ts
@@ -105,7 +105,7 @@ export class ChatPage {
         index = this.chat.messages.length - index - 1;
 
         const message = this.chat.messages[index];
-        const previousMessage = this.chat.messages[index - 1];
+        const previousMessage = this.chat.messages[index + 1];
 
         var classList: string[] = [];
 
